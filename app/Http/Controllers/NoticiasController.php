a10c60e82377f01544ba48ddf83f1eac72660f3c
Christopher Arthur <frapgameroficial@hotmail.com>
2021-07-05 14:11:38 -0300

    Att news/update Controller

--- app/Http/Controllers/NoticiasController.php
+++ app/Http/Controllers/NoticiasController.php
@@ -18,13 +18,16 @@ private $objNews;
     }
 
     public function store(Request $request) {
+
+        $date = new DateTime(date("d/m/Y"));
+
         $this->objNews->create([
             'id'=>$request->id,
             'title'=>$request->title,
             'backgroundimage'=>$request->backgroundimage,
             'topstory'=>$request->topstory,
             'longstory'=>$request->longstory,
-            'date'=>$request->date,
+            'date'=>$request->$date->date,
             'author'=>$request->author
         ]);
     }
@@ -32,5 +35,31 @@ private $objNews;
     public function destroy($id) {
         $del=$this->objNews->destroy($id);
         return($del)?"sim":"não";
+    }
+
+    public function update(Request $request, $id) {
+        if ($_POST['title'] == $request->title) {
+            $this->objNews->where(['id'=>$id])->update([
+                'title'=>$request->title,
+            ]);
+        }
+
+        if ($_FILE['backgroundimage'] == $request->backgroundimage) {
+            $this->objNews->where(['id'=>$id])->update([
+                'backgroundimage'=>$request->backgroundimage
+            ]);
+        }
+
+        if ($POST['topstory'] == $request->topstory) {
+            $this->objNews->where(['id'=>$id])->update([
+                'topstory'=>$request->topstory
+            ]);
+        }
+
+        if ($POST['longstory'] == $request->longstory) {
+            $this->objNews->where(['id'=>$id])->update([
+                'longstory'=>$request->longstory
+            ]);
+        }
     }
 }
